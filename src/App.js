0bb2b99a724bf3bb6baafd437ad273b9394b2b58
Luke Schuyler <lukeschuyler1@gmail.com>
2018-11-04 14:23:17 -0800

    updated docker, testing delpy again

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            SUUUUUHHHHHH
+            SUUUUUHHHHHHHERRRo
           </p>
           <a
             className="App-link"
